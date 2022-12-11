b6382e43099a4a0978f6cc097fff87b0c812edac
Denis <Googlonator@gmail.com>
2022-12-11 17:28:16 +0400

    Layout header fix

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,21 +1,27 @@
-import React, {FC} from 'react'
+import  {FC} from 'react'
 import CartBtn from './CartBtn/CartBtn'
 import Find from './Find/Find'
 import Location from './Location/Location'
 import ProfilePhoto from './ProfilePhoto/ProfilePhoto'
 import s from './Header.module.css'
 import Logo from '../common/Logo/Logo'
+import {useMediaQuery} from 'react-responsive'
 
 interface HeaderProps {
 }
 
 const Header: FC<HeaderProps> = ({}) => {
-  return <div className={s.header}>
-    <Logo />
-    <Location />
+  const isMaxWidth = useMediaQuery({
+    query: "(max-width: 600px)",
+  });
+
+
+  return <div className={s.header + ' header'}>
+    {isMaxWidth || <Logo />}
+    {isMaxWidth  ||  <Location /> }
     <Find />
     <CartBtn />
-    <ProfilePhoto />
+    {isMaxWidth || <ProfilePhoto />}
   </div>
 }
 
