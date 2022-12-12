1dcd734bee8e79f46bb9f51d60bc478fd6648021
Denis <Googlonator@gmail.com>
2022-12-12 19:39:19 +0400

    create elements in mobile menu

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,36 +4,44 @@ import Find from "./Find/Find";
 import Location from "./Location/Location";
 import ProfilePhoto from "./ProfilePhoto/ProfilePhoto";
 import Logo from "../common/Logo/Logo";
-import s from './Header.module.css'
-import {useAppSelector} from "../../hooks/useAppSelector";
-import {useAppDispatch} from "../../hooks/useAppDispatch";
-import {closeMobileMenu, setMenuIsOpen} from "../../store/slices/commonSlice";
-import {handlerClickOutside} from "../../utilites/clickHandlerOutsideElement";
+import s from "./Header.module.css";
+import { useAppSelector } from "../../hooks/useAppSelector";
+import { useAppDispatch } from "../../hooks/useAppDispatch";
+import { closeMobileMenu, setMenuIsOpen } from "../../store/slices/commonSlice";
+import { handlerClickOutside } from "../../utilites/clickHandlerOutsideElement";
+import { NavLink } from "react-router-dom";
+import { useMediaQuery } from "react-responsive";
 
 interface HeaderProps {
-  mobileBtn: React.MutableRefObject<HTMLDivElement>
+  mobileBtn: React.MutableRefObject<HTMLDivElement>;
 }
 
-const Header: FC<HeaderProps> = ({mobileBtn}) => {
-  const menuIsOpen = useAppSelector(state => state.commonSlice.menuIsOpen)
-  const dispatch = useAppDispatch()
-  const headerRef = useRef() as React.MutableRefObject<HTMLDivElement>
+const Header: FC<HeaderProps> = ({ mobileBtn }) => {
+  const menuIsOpen = useAppSelector((state) => state.commonSlice.menuIsOpen);
+  const dispatch = useAppDispatch();
+  const headerRef = useRef() as React.MutableRefObject<HTMLDivElement>;
+  const isDesktopOrLaptop = useMediaQuery({
+    query: "(max-width: 600px)",
+  });
 
   const closeMenu = () => {
-    dispatch(closeMobileMenu()) 
-  }
+    dispatch(closeMobileMenu());
+  };
 
   const unFocus = (e: any) => {
-    handlerClickOutside(e, [headerRef, mobileBtn], closeMenu )
-  }
+    handlerClickOutside(e, [headerRef, mobileBtn], closeMenu);
+  };
 
-  useEffect(()=> {
-    document.addEventListener('mousedown', unFocus)
-    return () => document.removeEventListener('mousedown', unFocus)
-  }, [])
+  useEffect(() => {
+    document.addEventListener("mousedown", unFocus);
+    return () => document.removeEventListener("mousedown", unFocus);
+  }, []);
 
   return (
-    <div ref={headerRef} className={s.header + ' ' + (menuIsOpen ? s.open : '')}>
+    <div
+      ref={headerRef}
+      className={s.header + " " + (menuIsOpen ? s.open : "")}
+    >
       <Logo />
 
       <div className={s["header__wrap"]}>
@@ -42,6 +50,17 @@ const Header: FC<HeaderProps> = ({mobileBtn}) => {
         <div className={s.wrap}>
           <CartBtn />
         </div>
+        {isDesktopOrLaptop && (
+          <div className={s.history}>
+            <NavLink onClick={() => closeMenu()} to={"/shop"}>
+              {" "}
+              На главную
+            </NavLink>
+            <NavLink onClick={() => closeMenu()} to="/history">
+              История заказов
+            </NavLink>
+          </div>
+        )}
         <ProfilePhoto />
       </div>
     </div>
