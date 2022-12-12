59967ca928e11b31898a850e7c63fc061473d28c
Denis <Googlonator@gmail.com>
2022-12-12 16:45:57 +0400

    Create func buy btn in cart

--- src/components/Content/Cart/Bar/Bar.tsx
+++ src/components/Content/Cart/Bar/Bar.tsx
@@ -1,16 +1,24 @@
-import React, { FC } from "react";
+import  { FC } from "react";
+import {useNavigate} from "react-router-dom";
 import s from './Bar.module.css'
 import Price from "./Price/Price";
 
 interface BarProps {}
 
 const Bar: FC<BarProps> = ({}) => {
+  const navigate = useNavigate()
+  
+
+  const orderFunc = () => {
+    navigate('/delivery')
+  } 
+
   return (
     <div className={s.bar}>
       <h3 className={s["product-name"]}>Xiaomi</h3>
       <div className={s.wrap}>
  <Price />
-      <input className={s.btn + ' blue-btn'} type="button" value="Оформить" />
+ <input onClick={()=>orderFunc()} className={s.btn + ' blue-btn'} type="button" value="Оформить" />
 
       </div>
          </div>
