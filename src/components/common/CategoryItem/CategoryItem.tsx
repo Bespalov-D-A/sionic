43370b699f822a9b69ced674a64745c09059d34d
Denis <Googlonator@gmail.com>
2022-12-14 16:29:35 +0400

    Fix delivery form minor change

--- src/components/common/CategoryItem/CategoryItem.tsx
+++ src/components/common/CategoryItem/CategoryItem.tsx
@@ -1,25 +1,35 @@
-import  {FC} from 'react'
-import {useAppDispatch} from '../../../hooks/useAppDispatch';
-import {setSelectedCategory} from '../../../store/slices/commonSlice';
-import {ICategory} from '../../../types/productTypes';
-import s from './CategoryItem.module.css'
+import { FC } from "react";
+import { useAppDispatch } from "../../../hooks/useAppDispatch";
+import { useAppSelector } from "../../../hooks/useAppSelector";
+import { setSelectedCategory } from "../../../store/slices/commonSlice";
+import { ICategory } from "../../../types/productTypes";
+import s from "./CategoryItem.module.css";
 
 interface CategoryItemI {
   category: ICategory;
 }
 
-const CategoryItem: FC<CategoryItemI> = ({category}) => {
-  const {id} = category
-  const dispatch = useAppDispatch()
+const CategoryItem: FC<CategoryItemI> = ({ category }) => {
+  const selectedCategory = useAppSelector(
+    (state) => state.commonSlice.selectedCategory
+  );
+  const { id } = category;
+  const dispatch = useAppDispatch();
 
   const setCategory = () => {
-    dispatch(setSelectedCategory(id))
-  }
+    dispatch(setSelectedCategory(id));
+  };
 
-  const {name} = category
+  const { name } = category;
 
-return <div onClick={setCategory} className={s.category}>{name}</div>
-}
+  return (
+    <div
+      onClick={setCategory}
+      className={s.category + " " + (selectedCategory === id ? s.active : "")}
+    >
+      {name}
+    </div>
+  );
+};
 
-
-export default CategoryItem
+export default CategoryItem;
